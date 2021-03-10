116a967fd0fce25ef4688c13314fe5fe1e33b83f
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-03-10 21:57:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 let costOfItemsAtTraderJoes =  [1, 2, 6, 7, 8]
   let costOfItemsAtWholeFoods =  [3, 4, 5, 9, 10]
   
@@ -8,6 +6,8 @@ let secondHalf =  [3, 4, 5, 9, 10]
 function findMin(firstHalf, secondHalf){
   let minfirstHalf = firstHalf[0]
   let minsecondHalf = secondHalf[0]
+
+function findMinAndRemoveSorted=(1,2,3,4); 
 
   if(minfirstHalf < minsecondHalf){
     return minfirstHalf
